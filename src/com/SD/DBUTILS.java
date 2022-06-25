67da8aa4fb548b7689e15c3748694a633dbd31bc
MD. Ismail <ismail220285@gmail.com>
2022-06-25 13:12:43 +0600

    LoginFrame and StudentFrame Done by C211035

---
+++ src/com/SD/DBUTILS.java
@@ -0,0 +1,37 @@
+package com.SD;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBUTILS {
+
+    private static String jdbcURL = "jdbc:mysql://localhost:3306/studentdb";
+    private static String jdbcUsername = "root";
+    private static String jdbcPassword = "root";
+
+    public static Connection getConnection() {
+        
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); 
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static ResultSet queryExecute(String query) throws Exception{
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        return stmt.executeQuery(query);
+    }
+    
+    public static int queryUpdate(String query) throws Exception{
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        return stmt.executeUpdate(query);
+    }
+}
