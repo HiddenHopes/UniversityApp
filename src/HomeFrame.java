2eaf5b2839d6fef1cc52cbc9cb4db6c5086dea7a
unknown <faisalhossen.cse@gmail.com>
2022-07-06 17:29:18 +0600

    student frame done by c211021

--- src/HomeFrame.java
+++ src/HomeFrame.java
@@ -126,7 +126,9 @@ public class HomeFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code here
+        this.setVisible(false);
+        new StudentFrame().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
